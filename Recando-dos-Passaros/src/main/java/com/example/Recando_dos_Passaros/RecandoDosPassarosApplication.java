b84409436c9dcf0a441b7a997e1409a54fb35a63
Nandotech99 <fernando.pjuliano@senacsp.edu.br>
2026-09-23 10:26:37 -0300

    Versao 1

---
+++ Recando-dos-Passaros/src/main/java/com/example/Recando_dos_Passaros/RecandoDosPassarosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Recando_dos_Passaros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecandoDosPassarosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecandoDosPassarosApplication.class, args);
+	}
+
+}
---
+++ Recando-dos-Passaros/src/test/java/com/example/Recando_dos_Passaros/RecandoDosPassarosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Recando_dos_Passaros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecandoDosPassarosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
